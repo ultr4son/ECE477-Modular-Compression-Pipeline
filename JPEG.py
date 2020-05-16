3228f9896ea4a456b0f4722394f8524805333a7f
everettbrandt <56739587+everettbrandt@users.noreply.github.com>
2020-05-16 16:55:21 -0700

    Add files via upload

--- JPEG.py
+++ JPEG.py
@@ -5,20 +5,25 @@ class Jpeg:
     def __init__(self, input):
         self.input = input
         return
+    
+    def update_img(self, input):
+        self.input = input
+        return
 
     #Quality range is 0 to 95
-    def encode(self, img, output_file, quality):
-        cv2.imwrite(output_file, img, [cv2.IMWRITE_JPEG_QUALITY, quality])    
+    def encode(self, output_file, quality):
+        cv2.imwrite(output_file, self.input, [cv2.IMWRITE_JPEG_QUALITY, quality])    
         return os.path.getsize(output_file)
 
-    def decode(self, input_file, output_file):
-        cv2.imwrite(output_file, img)
+    def decode(self, output_file):
+        cv2.imwrite(output_file, self.input)
         return os.path.getsize(output_file)
 
 if __name__ == '__main__':    
     #Testing Jpeg class
     img = cv2.imread('MARBLES.BMP')
     transform = Jpeg(img)
-    print(transform.encode(img, "MARBLES.jpg", 50))
+    print(transform.encode("MARBLES.jpg", 10))
     img = cv2.imread("MARBLES.jpg")
-    print(transform.decode(img, "MARBLES2.BMP"))
+    transform.update_img(img)
+    print(transform.decode("MARBLES2.BMP"))
